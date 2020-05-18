be80c191adea10bd2a3b2bf48bf6c655c90a63a7
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-18 11:32:37 -0300

    Add AppBar e TextField para search.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -9,13 +9,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String search;
+  String _search;
   int _offset = 0;
 
   Future<Map> _getGifs() async {
     http.Response response;
 
-    if (search == null) {
+    if (_search == null) {
       response = await http.get(
           "https://api.giphy.com/v1/gifs/trending?api_key=Fl1PpeptBKCt6ItETdYPr9QOFBbvFFN4&limit=20&rating=G");
     } else {
@@ -37,6 +37,29 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Pesquise aqui!",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
