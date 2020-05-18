6bc283968c6daf93c14c539f428d1bf5cbe21600
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-18 11:34:01 -0300

    Add tema para correções de bordas e outros campos.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
-    theme: ThemeData(hintColor: Colors.white),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
   ));
 }
